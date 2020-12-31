a58c2323a12c1925e10c3ac538e75492cc67fcf7
manvendra09 <ankursingh297@gmail.com>
2020-12-31 14:44:05 +0530

    api request added

--- covid19app/views.py
+++ covid19app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
+import requests
 # Create your views here.
 
 def index(request):
-	return HttpResponse("<h1>Hello World</h1>")
+	g_summmay = None
+	data = True
+	result = None
+    #globalsummary = None
+	while(data):
+		try:
+			result = requests.get('https://api.covid19api.com/summary') 
+			g_summmay = result.json()['Global']
+			data = False
+		except:
+			data = True
+		
+	return render(request, 'index.html', {'g_summmay' : g_summmay})
+	
